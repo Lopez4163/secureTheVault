360e9221569a9af8dfee65165cd519b4d7edde5a
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-03-04 13:34:32 -0600

    completed

---
+++ index.js
@@ -0,0 +1,10 @@
+//MAKE VARIABLES THAT CONTAIN EACH COMBO KEY USING
+//CONCAT
+const code1 = `${9 + 1}`
+const code2 = `${8 * 5}`
+const code3 = `${40 - 1}`
+//MAKE A VARIABLE THAT CONTAINS THE FULL COMBO
+const combination = `${code1}-${code2}-${code3}`
+console.log(combination)
+//ALERT TO DISPLAY THE COMB
+alert(combination)
