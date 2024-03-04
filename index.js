c3016f65de2611e9c0ef34b9887171cf0f4cb3b6
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2024-03-04 15:04:37 -0600

    completed

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const combination = `${code1}-${code2}-${code3}`
 console.log(combination)
 //ALERT TO DISPLAY THE COMB
 alert(combination)
+//DISPLAY THE COMBO IN THE DOM
+//JUST PRACTICING DOM MANIPULATION
+
+//ACCESS H1 IN DOM
+const codeView1 = document.getElementById("codeView1")
+const codeView2 = document.getElementById("codeView2")
+const codeView3 = document.getElementById("codeView3")
+
+const combinationHeading = document.getElementById("combinationHeading")
+
+//UPDATE DOM
+codeView1.textContent = code1
+codeView2.textContent = code2
+codeView3.textContent = code3
+combinationHeading.textContent = combination
